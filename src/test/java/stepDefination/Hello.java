8af4de232e03d41e494e4f02e16a5fd88a85413e
Praveen M <praveen.mohansundar9@gmail.com>
2025-07-23 17:02:30 +0530

    hello

---
+++ src/test/java/stepDefination/Hello.java
@@ -0,0 +1,10 @@
+package stepDefination;
+
+public class Hello{
+    public static void main(String[] args) {
+               System.out.println("from Main branch");
+       System.out.println("from branch");
+
+       System.out.println("from praveen branch");
+    }
+}
